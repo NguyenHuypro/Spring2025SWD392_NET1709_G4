59a51585c767eb3f85d334d2c5e2449372192734
tungnk <tungnk1404@gmail.com>
2025-03-20 23:13:55 +0700

    update axios

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const baseURL = import.meta.env.VITE_BASE_URL;
-const baseURL = "http://localhost:3000/api";
+const baseURL = import.meta.env.VITE_BASE_URL;
+// const baseURL = "http://localhost:3000/api";
 
 const config = {
   baseURL,
