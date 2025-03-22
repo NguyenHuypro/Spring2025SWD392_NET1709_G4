2d73e9e3e843d692eec899e888175dfec4ddce72
tungnk <tungnk1404@gmail.com>
2025-03-22 23:17:54 +0700

    add payment history

--- src/pages/user/payment-history/index.jsx
+++ src/pages/user/payment-history/index.jsx
@@ -1,7 +1,8 @@
-import { Table } from "antd";
+import { Table, Tag } from "antd";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import api from "../../../configs/axios";
+import { changeCurr } from "../../../utils/utils";
 
 function PaymentHistory() {
   const [dataSource, setDataSource] = useState([]);
@@ -11,11 +12,61 @@ function PaymentHistory() {
       dataIndex: "id",
       key: "id",
     },
+    {
+      title: "Thanh toán cho",
+      render: (value, record) => {
+        if (record.packageName) {
+          return "Gói dịch vụ";
+        }
+        return "Cứu hộ";
+      },
+    },
+    {
+      title: "Xe",
+      dataIndex: "licensePlate",
+      key: "licensePlate",
+    },
+    {
+      title: "Tổng tiền",
+      dataIndex: "amount",
+      key: "amount",
+      render: (value) => changeCurr(value),
+    },
+    {
+      title: "Trạng thái",
+      dataIndex: "status",
+      key: "status",
+      render: (value) => {
+        let color = "";
+        let text = "";
+
+        switch (value) {
+          case "pending":
+            color = "gold";
+            text = "Đang xử lý";
+            break;
+          case "success":
+            color = "green";
+            text = "Thành công";
+            break;
+          case "failed":
+            color = "red";
+            text = "Thất bại";
+            break;
+          default:
+            color = "default";
+            text = value;
+        }
+
+        return <Tag color={color}>{text}</Tag>;
+      },
+    },
   ];
 
   const fetchTransactions = async () => {
     try {
       const res = await api.get("/my-transactions");
+      console.log(res.data.result);
       setDataSource(res.data.result);
     } catch (error) {
       toast.error(error.message);
